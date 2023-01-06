45447c8ef4dd18e82e2fb1f79f65b41cfa70592d
Michael Paukov <michael.paukov@gmail.com>
2023-01-06 15:04:18 +0100

    fix currency

--- src/api/currencyAPI.js
+++ src/api/currencyAPI.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+
 async function fetchCurrency() {
-  const { data } = await axios('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+  const { data } = await axios(`https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`);
 
   return data;
 }

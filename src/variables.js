0578f9b08dc2d00776860f90983e0d13553a1817
Michael Paukov <michael.paukov@gmail.com>
2023-01-07 14:16:27 +0100

    change guest accaunt

--- src/variables.js
+++ src/variables.js
@@ -2,4 +2,4 @@
 // export const BASE_URL = 'https://wallet-proj.herokuapp.com';
 export const BASE_URL = 'https://wallet-api-back.vercel.app';
 
-export const TEST_USER = { email: 'guest@gmail.com', password: 'f8kd8ddld9dd' };
+export const TEST_USER = { email: 'guest@test.com', password: '6HF2r4cs7yn8SB9' };
